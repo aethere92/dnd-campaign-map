249eef05c15babdecad878326daa827532ec16f7
Adrian Iana <adrianiana@live.com>
2024-10-31 16:20:54 +0200

    Added world map to Korinis Landing. Added session recap 1 to unnamed island.

--- src/databases/MapsDb.js
+++ src/databases/MapsDb.js
@@ -1680,6 +1680,14 @@ const MAP_DATABASE = {
 												icon: 'cityCapital',
 												iconType: 'png',
 											},
+											{
+												lat: -174.72,
+												lng: 24.25,
+												label: 'World Map',
+												type: 'text',
+												fontSize: 14,
+												mapLink: 'world_maps',
+											},
 										],
 									},
 									navigation_arrows: {
@@ -1820,9 +1828,182 @@ const MAP_DATABASE = {
 									iconType: 'png',
 									mapLink: 'world_maps.submaps.islands.unnamed_island_01.submaps.encounters.finneas_encounter',
 								},
+								{
+									lat: -193.5,
+									lng: 241.5,
+									label: 'Dinosaurs combat encounter',
+									type: 'place',
+									icon: 'poiCombat',
+									iconType: 'png',
+								},
+							],
+						},
+						points_of_interest: {
+							name: 'Points of Interest',
+							items: [
+								{
+									lat: -326.25,
+									lng: 206.75,
+									label: 'Landing zone',
+									type: 'place',
+									icon: 'cityCapital',
+									iconType: 'png',
+								},
 							],
 						},
 					},
+					paths: [
+						{
+							name: 'Session 1 Recap',
+							points: [
+								{
+									coordinates: [-329.5, 180.75],
+									text: 'Landed on beach.',
+								},
+								{
+									coordinates: [-289.5, 222.5],
+								},
+								{
+									coordinates: [-289.5, 222.5],
+									text: 'Went up to the mountain path',
+								},
+								{
+									coordinates: [-272, 227],
+								},
+								{
+									coordinates: [-266.5, 214.5],
+								},
+								{
+									coordinates: [-260, 206],
+								},
+								{
+									coordinates: [-253.5, 212],
+								},
+								{
+									coordinates: [-250.5, 223.5],
+								},
+								{
+									coordinates: [-247.5, 229],
+								},
+								{
+									coordinates: [-240, 232],
+								},
+								{
+									coordinates: [-232.5, 232],
+								},
+								{
+									coordinates: [-224, 226.5],
+								},
+								{
+									coordinates: [-215.5, 226.5],
+								},
+								{
+									coordinates: [-209.5, 235],
+								},
+								{
+									coordinates: [-198.5, 229],
+								},
+								{
+									coordinates: [-193.5, 241.5],
+									text: 'Fought dinosaurs',
+								},
+								{
+									coordinates: [-171, 244],
+								},
+								{
+									coordinates: [-163, 257],
+								},
+								{
+									coordinates: [-154, 271],
+								},
+								{
+									coordinates: [-143, 277.5],
+								},
+								{
+									coordinates: [-138.5, 286],
+								},
+								{
+									coordinates: [-150.5, 289],
+								},
+								{
+									coordinates: [-162, 296],
+									text: 'Reached lake.',
+								},
+								{
+									coordinates: [-147.5, 306],
+									text: 'Climbed onto platforms.',
+								},
+								{
+									coordinates: [-126.625, 314.75],
+									text: 'Entered door, met Finneas, fought undeads.',
+								},
+								{
+									coordinates: [-143.5, 324.5],
+								},
+								{
+									coordinates: [-170, 305.5],
+									text: 'Left, shot flare to notify the crew we found water.',
+								},
+								{
+									coordinates: [-176, 289.5],
+								},
+								{
+									coordinates: [-183, 277.5],
+								},
+								{
+									coordinates: [-197.5, 276],
+									text: 'Took another path down the mountain, found dead crew member parties bodies and looted them.',
+								},
+								{
+									coordinates: [-209.5, 281.5],
+								},
+								{
+									coordinates: [-215, 274.5],
+								},
+								{
+									coordinates: [-222.5, 251.5],
+								},
+								{
+									coordinates: [-222.5, 238.5],
+								},
+								{
+									coordinates: [-225.75, 234],
+								},
+								{
+									coordinates: [-236.5, 237.625],
+								},
+								{
+									coordinates: [-248, 235],
+								},
+								{
+									coordinates: [-254.625, 218.25],
+								},
+								{
+									coordinates: [-262.5, 218],
+								},
+								{
+									coordinates: [-273.25, 232.75],
+								},
+								{
+									coordinates: [-298.625, 230],
+									text: 'Saw the ship preparing to depart without waiting for us.',
+								},
+								{
+									coordinates: [-317.375, 218.375],
+								},
+								{
+									coordinates: [-317.375, 218.375],
+								},
+								{
+									coordinates: [-346.875, 193.75],
+								},
+								{
+									coordinates: [-351.75, 125.25],
+									text: "Made a run for it and reached the ship back in time so we didn't get left behind.",
+								},
+							],
+						},
+					],
 					submaps: {
 						encounters: {
 							finneas_encounter: {
