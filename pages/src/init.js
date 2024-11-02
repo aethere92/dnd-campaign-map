af4a29084d4cc2dd23a904d16e74e53483d9e108
Adrian Iana <adrianiana@live.com>
2024-11-02 18:34:21 +0200

    Added code to update type from schemas.

--- pages/src/init.js
+++ pages/src/init.js
@@ -2,6 +2,17 @@ let MAP_DATABASE = null;
 let currentPath = [];
 let searchResults = new Set();
 
+document.getElementById('new-item-dialog').querySelector('select').innerHTML = `
+                <option value="string">String</option>
+                <option value="number">Number</option>
+                <option value="boolean">Boolean</option>
+                <option value="object">Object</option>
+                <option value="array">Array</option>
+                ${Object.keys(PREDEFINED_SCHEMAS)
+									.map((key) => `<option value="schema_${key}">${key} (Schema)</option>`)
+									.join('')}
+            `;
+
 // Show import dialog
 function showImportDialog() {
 	document.getElementById('import-dialog').showModal();
