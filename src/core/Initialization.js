8def8cb4c437d9164e7a105643655e634e7d1d68
Adrian Iana <adrianiana@live.com>
2024-11-20 15:26:45 +0200

    Removed debug flag.

--- src/core/Initialization.js
+++ src/core/Initialization.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
 	// const map = new CustomMap('map', 'world_maps', false);
-	const map = new CustomMap('map', 'korinis_island', true);
+	const map = new CustomMap('map', 'korinis_island', false);
 });
